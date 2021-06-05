6c92705fe007609f65341a291e25fce596c2772b
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 12:25:49 +0200

    stylesWrapper HOC design pattern implemented.

---
+++ src/styles/commonStyles.js
@@ -0,0 +1,14 @@
+
+const styles = {
+    default : {
+        backgroundColor: '#d1135f',
+        color: '#ffffff',
+        padding: '10px'
+    },
+    disable : {
+        backgroundColor: '#ffff00',
+        color: '#ff00ff'
+    }
+}
+
+export default styles;
